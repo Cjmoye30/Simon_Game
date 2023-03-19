c389d4acf6ae1927690e8215bf9840241aad1302
CJMoye <Cjmoye30@gmail.com>
2023-03-19 14:25:33 -0400

    MVP met

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ```
 The game starts when the start button is clicked
+
 When the game starts, the index is increased to 1, and then the computer array is added one variable from the randomly generated color with the math functions.
 
 When the game starts, the title element is updated to the current level/index of the game
--- index.js
+++ index.js
@@ -1,17 +1,78 @@
 // Variables
 const colors = ["red", "blue", "green", "yellow"];
+let computerArr = [];
+const maxLevel = 20;
 let level = 0;
+let displayIndex = 0;
+let checkIndex = 0;
 
-
-// Event Listeners for buttons
-// When the button is clicked, then a sound is played
-$(".col").click(function(e){
-    let userSelect = e.target.id;
-    console.log(userSelect);
-
-    // Play the correct sound of the button selected
-    var audio = new Audio("assets/sounds/"+userSelect+".mp3")
-    audio.play();
+// Event Listeners
+$("#startButton").click(function () {
+    level = 1;
+    $("#titleElement").text("Level: " + level);
+    $("#startButton").attr("style", "display: none");
+    playGame();
 })
 
+$(".col").click(function (e) {
+    effects(e.target.id);
+    if (e.target.id === computerArr[checkIndex]) {
+        console.log("Correct!");
+        checkIndex++;
+        console.log(level, checkIndex);
+        if(level === checkIndex) {
+            console.log("Move to the next level");
+            level++;
+            console.log(level);
+            setTimeout(function(){
+                $("#titleElement").text("Level: "+level);
+                playGame();
+            },500)
+        }
+    } else {
+        console.log("Incorrect!");
+        $("#titleElement").text("Wrong!");
+    }
+})
 
+// Functions
+function generateGamePattern() {
+    for (let i = 0; i < maxLevel; i++) {
+        let randNum = Math.floor(Math.random() * colors.length);
+        let randColor = colors[randNum];
+        computerArr.push(randColor);
+    }
+    console.log(computerArr);
+}
+
+function showPattern() {
+    setTimeout(function(){
+        console.log(computerArr[displayIndex]);
+        effects(computerArr[displayIndex]);
+        displayIndex++;
+        if(displayIndex < level) {
+            showPattern();
+        }
+    },500)
+}
+
+function playGame() {
+    checkIndex = 0;
+    displayIndex = 0;
+    showPattern();
+}
+
+function effects(x) {
+    // Flash buttons on click or computer generate
+    $("#" + x).attr("style", "background-color: whitesmoke");
+    setTimeout(function () {
+        $("#" + x).removeAttr("style", "background-color: whitesmoke");
+    }, 100);
+
+    // Play sound of button click or computer generate
+    var audio = new Audio("assets/sounds/" + x + ".mp3")
+    audio.play();
+}
+
+// Create game pattern on document load
+generateGamePattern();
